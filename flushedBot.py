f542a27d077bd0bce6a446cc0d0076689f42fe32
Quancong Ni <niquancong@gmail.com>
2020-01-21 01:38:08 -0500

    implemented :flushed: cases for 'i'm in ', 'i am in ', and 'im in '

--- flushedBot.py
+++ flushedBot.py
@@ -5,13 +5,58 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix = '...?')
 
-config_file = 'config.json'
-with open('config.json') as json_file:
-    data = json.load(json_file)
+with open('config.json') as config_file:
+
+    data = json.load(config_file)
     token = data['token']
     
 @client.event
+
 async def on_ready():
+
     print('Bot is ready.')
 
+@client.event
+
+async def on_message(message):
+
+    cond1 = 'i\'m in ' in message.content.lower()
+    cond2 = 'i am in ' in message.content.lower()
+    cond3 = 'im in ' in message.content.lower()
+    output = message.content
+    
+    flushed = False
+    
+    while(cond1 or cond2 or cond3):   
+        
+        if cond1:
+
+            j = output.find('i\'m in ') - len(output)
+            output = output[j + 7:]
+            if output.count('i\m in ') == 0:
+                cond1 = False
+                flushed = True
+                
+        if cond2:
+        
+            j = output.find('i am in ') - len(output)
+            output = output[j + 8:]
+            if output.count('i am in ') == 0:
+                cond2 = False
+                flushed = True
+                
+        if cond3:
+        
+            j = output.find('im in ') - len(output)
+            output = output[j + 6:]
+            if output.count('im in ') == 0:
+                cond3 = False
+                flushed = True
+                 
+    if flushed:
+    
+        channel = message.channel
+        await channel.send(f'i\'m {output} :flushed:')
+
 client.run(token)
+
